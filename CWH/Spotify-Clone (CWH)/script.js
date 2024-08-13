91fc17dd8d66f6c482b436f02e17ebc7f1d9db7c
Jamalshah <jimisk811@gmail.com>
2024-08-13 08:15:52 +0530

    done

--- CWH/Spotify-Clone (CWH)/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Lets write javascript");
-
-async function main() {
-  let a = await fetch("http://127.0.0.1:5500/CWH/Spotify-Clone%20(CWH)/songs/");
-  let response = await a.text();
-  console.log(response);
-}
-
-main();
