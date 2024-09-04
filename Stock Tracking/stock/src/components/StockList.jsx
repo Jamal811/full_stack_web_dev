1235b835e2de55bffbb08ed217c8dddcc6a63724
Jamalshah <jimisk811@gmail.com>
2024-09-04 17:58:58 +0530

    updates

--- Stock Tracking/stock/src/components/StockList.jsx
+++ Stock Tracking/stock/src/components/StockList.jsx
@@ -1,3 +1,32 @@
+import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
 export default function StockList() {
+  const [stock, setStock] = useState();
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const responses = await Promise.all(
+          watchList.map((stock) => {
+            return finnHub.get("/quote", {
+              params: {
+                symbol: stock,
+              },
+            });
+          })
+        );
+        console.log(responses);
+        if (isMounted) {
+          setStock(responses.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    return () => (isMounted = false);
+  }, []);
+
   return <div>StockList</div>;
 }
